9f9c1af31103f1da375d0b0aad6b56e0a836acf0
PashcalE2 <74344847+PashcalE2@users.noreply.github.com>
2024-11-11 18:39:35 +0300

    наверно так

--- cryptography/first_part/lab1.py
+++ cryptography/first_part/lab1.py
@@ -44,11 +44,13 @@ class CaesarMethod:
             not_rotated = True
             for alphabet in self.alphabets:
                 if alphabet.is_lower(char):
-                    result += alphabet.lowers[(ord(char) - alphabet.first_lower_ord + n) % alphabet.size]
+                    new_ord = (ord(char) - alphabet.first_lower_ord + n) % alphabet.size
+                    result += alphabet.lowers[new_ord]
                     not_rotated = False
                     break
                 elif alphabet.is_upper(char):
-                    result += alphabet.uppers[(ord(char) - alphabet.first_upper_ord + n) % alphabet.size]
+                    new_ord = (ord(char) - alphabet.first_upper_ord + n) % alphabet.size
+                    result += alphabet.uppers[new_ord]
                     not_rotated = False
                     break
 
@@ -64,6 +66,59 @@ class CaesarMethod:
         return False
 
 
+def freq_analysis(encrypted: str) -> dict:
+    chars_freq = {}
+    for i in range(len(encrypted)):
+        char = encrypted[i].lower()
+        if _caesar_method.in_alphabet(char):
+            if char in chars_freq:
+                chars_freq[char] += 1
+            else:
+                chars_freq[char] = 1
+
+    return chars_freq
+
+
+def find_key_words(text: str, key_words: list[str]):
+    result = []
+    for word in key_words:
+        if word.lower() in text.lower():
+            result.append(word)
+    return result
+
+
+def hack_caesar(
+        caesar_method: CaesarMethod,
+        encrypted: str,
+        key_words: list[str],
+        max_rotations: int) -> str:
+    acceptable_key_words_count = len(key_words) // 2
+
+    for n in range(1, max_rotations):
+        rotation = -n
+        print(f"Сдвиг на: {rotation}")
+
+        decrypted = caesar_method.rotate(encrypted, rotation)
+
+        chars_freq = freq_analysis(decrypted)
+        df = pd.DataFrame(
+            data=[[
+                key,
+                chars_freq[key],
+                chars_freq[key] / sum(chars_freq.values())
+            ] for key in chars_freq],
+            columns=["Буква", "Количество", "Вероятность"])
+        df = df.sort_values("Буква")
+        print(df)
+
+        found_key_words = find_key_words(decrypted, key_words)
+        found_key_words_count = len(found_key_words)
+        print(f"Найденные ключевые слова ({found_key_words_count}): {found_key_words}\n")
+
+        if found_key_words_count >= acceptable_key_words_count:
+            return decrypted
+
+
 _RU = Alphabet(
     first_lower_ord=ord("а"),
     first_upper_ord=ord("А"),
@@ -76,39 +131,30 @@ _EN = Alphabet(
 
 _caesar_method = CaesarMethod([_RU, _EN])
 
+_RU_key_words = ["и", "в", "то", "том", "на", "из", "под", "нам", "вам"]
+_EN_key_words = ["and", "in", "that", "on", "from", "of", "the", "a", "you", "we"]
+
+_key_words = _RU_key_words + _EN_key_words
+_max_rotations = max(_EN.size, _RU.size)
+
 
 def lab1(filepath: str, rotates_count: int):
     with open(filepath, "r", encoding="UTF-8") as file:
         filedata = file.read()
 
+    def get_part_from_text(text: str):
+        count = min(200, max(len(encrypted) // 2, 1000))
+        return text[:count // 2] + "\n...\n" + text[-count // 2:]
+
     encrypted = _caesar_method.rotate(filedata, rotates_count)
+    print(f"Зашифрованный текст:\n{get_part_from_text(encrypted)}")
     with open("./encrypted.txt", "w", encoding="UTF-8") as file:
         file.write(encrypted)
 
-    decrypted = _caesar_method.rotate(encrypted, -rotates_count)
+    decrypted = hack_caesar(_caesar_method, encrypted, _key_words, _max_rotations)
+    print(f"Расшифрованный текст:\n{get_part_from_text(decrypted)}")
     with open("./decrypted.txt", "w", encoding="UTF-8") as file:
         file.write(decrypted)
-
-    words_freq = {}
-    left_border = None
-    for i in range(len(encrypted)):
-        if _caesar_method.in_alphabet(encrypted[i]):
-            if left_border is None:
-                left_border = i
-        elif left_border is not None:
-            word = encrypted[left_border:i]
-            left_border = None
-
-            if word in words_freq:
-                words_freq[word] += 1
-            else:
-                words_freq[word] = 1
-
-    df = pd.DataFrame(
-        data=[[key, words_freq[key], words_freq[key] / sum(words_freq.values())] for key in words_freq],
-        columns=["Слово", "Количество", "Вероятность"])
-
-    print(df.to_string())
 
 
 if __name__ == "__main__":
