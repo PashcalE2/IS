d5e958e3c9b0275da76c34e7c95170743a8bc2fd
PashcalE2 <74344847+PashcalE2@users.noreply.github.com>
2024-11-15 18:53:27 +0300

    .

--- cryptography/first_part/lab2.py
+++ cryptography/first_part/lab2.py
@@ -1,21 +1,65 @@
 import random
 
 
-def lab2(filepath: str, K: list[int], IV: int):
-    with open(filepath, "r", encoding="UTF-8") as file:
-        filedata = file.read()
-
-    input_bytes = bytes(filedata, "UTF-8")
-    input_bytes = input_bytes + (b' ' * (len(input_bytes) % 8))
-
-    # блоки по 8 байт
-    N = []
-    for i in range(len(input_bytes) // 8):
-        N.append(int.from_bytes(input_bytes[i * 8:i * 8 + 8]))
-
-    
+_switch_table = [
+    [9, 6, 3, 2, 8, 11, 1, 7, 10, 4, 14, 15, 12, 0, 13, 5],
+    [3, 7, 14, 9, 8, 10, 15, 0, 5, 2, 6, 12, 11, 4, 13, 1],
+    [14, 4, 6, 2, 11, 3, 13, 8, 12, 15, 5, 10, 0, 7, 1, 9],
+    [14, 7, 10, 12, 13, 1, 3, 9, 0, 2, 11, 4, 15, 8, 5, 6],
+    [11, 5, 1, 9, 8, 13, 15, 0, 14, 4, 2, 3, 12, 7, 10, 6],
+    [3, 10, 13, 12, 1, 2, 0, 11, 7, 5, 9, 4, 8, 15, 14, 6],
+    [1, 13, 2, 9, 7, 10, 6, 0, 8, 12, 4, 5, 15, 3, 11, 14],
+    [11, 10, 15, 5, 0, 12, 14, 8, 6, 2, 3, 9, 1, 7, 13, 4]
+]
 
 
+def gost_main_block(N: int, K: int) -> int:
+    A = N & 0xFFFFFFFF
+    B = (N >> 32) & 0xFFFFFFFF
+
+    new_B = A
+    new_A = A ^ K
+    S = [(new_A >> i * 4) & 15 for i in range(8)]
+    for i, s in enumerate(S):
+        S[i] = _switch_table[i][s]
+
+    new_A = 0
+    for i in range(1, 9):
+        new_A = (new_A << 4) + S[-i]
+
+    new_A = ((new_A << 11) & 0xFFFFFFFF_FFFFFFFF) + (new_A >> 21)
+    new_A = B ^ new_A
+
+    return (new_B << 32) + new_A
+
+
+def gost_ofb(file_data: bytes, K: list[int], IV: int) -> bytes:
+    if len(file_data) & 7 != 0:
+        file_data = file_data + (b' ' * (8 - (len(file_data) & 7)))
+
+    iters_count = len(file_data) // 8
+
+    T = IV
+    result = []
+    for i in range(iters_count):
+        T = gost_main_block(T, K[i % len(K)])
+        text_block = int.from_bytes(file_data[i * 8:i * 8 + 8])
+        result.append(int.to_bytes(T ^ text_block, 8))
+
+    return b''.join(result)
+
+
+def lab4(input_file: str, encrypted_file: str, decrypted_file: str, K: list[int], IV: int):
+    with open(input_file, "r", encoding="UTF-8") as file:
+        input_data = file.read()
+
+    encrypted = gost_ofb(bytes(input_data, "UTF-8"), K, IV)
+    with open(encrypted_file, "wb") as file:
+        file.write(encrypted)
+
+    decrypted = gost_ofb(encrypted, K, IV)
+    with open(decrypted_file, "wb") as file:
+        file.write(decrypted)
 
 
 if __name__ == "__main__":
@@ -34,4 +78,4 @@ if __name__ == "__main__":
     _K = [random.randint(0, (2 ** 32) - 1) for i in range(8)]
     _IV = random.randint(0, (2 ** 64) - 1)
 
-    lab2("input.txt", _K, _IV)
+    lab4("input.txt", "encrypted.txt", "decrypted.txt", _K, _IV)
