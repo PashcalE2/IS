4091387c27f93480057277f29a6ecb7fa22b627f
PashcalE2 <74344847+PashcalE2@users.noreply.github.com>
2024-11-25 18:48:02 +0300

    .

--- cryptography/first_part/lab2.py
+++ cryptography/first_part/lab2.py
@@ -19,45 +19,66 @@ def gost_main_block(N: int, K: int) -> int:
 
     new_B = A
     new_A = A ^ K
-    S = [(new_A >> i * 4) & 15 for i in range(8)]
-    for i, s in enumerate(S):
-        S[i] = _switch_table[i][s]
+
+    S = [(new_A >> ((7 - i) * 4)) & 15 for i in range(8)]
+    for i in range(8):
+        S[i] = _switch_table[i][S[i]]
 
     new_A = 0
-    for i in range(1, 9):
-        new_A = (new_A << 4) + S[-i]
+    for i in range(8):
+        new_A = new_A | (S[i] << ((7 - i) * 4))
 
-    new_A = ((new_A << 11) & 0xFFFFFFFF_FFFFFFFF) + (new_A >> 21)
+    new_A = ((new_A << 11) & 0xFFFFFFFF) + (new_A >> 21)
     new_A = B ^ new_A
 
     return (new_B << 32) + new_A
 
 
-def gost_ofb(file_data: bytes, K: list[int], IV: int) -> bytes:
-    if len(file_data) & 7 != 0:
-        file_data = file_data + (b' ' * (8 - (len(file_data) & 7)))
-
+def gost_ofb_encrypt(file_data: bytes, K: list[int], IV: int) -> bytes:
     iters_count = len(file_data) // 8
 
     T = IV
-    result = []
+    result = b''
     for i in range(iters_count):
         T = gost_main_block(T, K[i % len(K)])
-        text_block = int.from_bytes(file_data[i * 8:i * 8 + 8])
-        result.append(int.to_bytes(T ^ text_block, 8))
 
-    return b''.join(result)
+        text_block = int.from_bytes(file_data[i * 8:(i + 1) * 8])
+        T = T ^ text_block
+
+        result += int.to_bytes(T, 8)
+
+    return result
+
+
+def gost_ofb_decrypt(file_data: bytes, K: list[int], IV: int) -> bytes:
+    iters_count = len(file_data) // 8
+
+    T = IV
+    result = b''
+    for i in range(iters_count):
+        T = gost_main_block(T, K[i % len(K)])
+
+        text_block = int.from_bytes(file_data[i * 8:(i + 1) * 8])
+        result += int.to_bytes(T ^ text_block, 8)
+
+        T = text_block
+
+    return result
 
 
 def lab4(input_file: str, encrypted_file: str, decrypted_file: str, K: list[int], IV: int):
     with open(input_file, "r", encoding="UTF-8") as file:
         input_data = file.read()
 
-    encrypted = gost_ofb(bytes(input_data, "UTF-8"), K, IV)
+    input_bytes = bytes(input_data, "UTF-8")
+    if len(input_bytes) % 8 > 0:
+        input_bytes = input_bytes + (b' ' * (8 - (len(input_bytes) % 8)))
+
+    encrypted = gost_ofb_encrypt(input_bytes, K, IV)
     with open(encrypted_file, "wb") as file:
         file.write(encrypted)
 
-    decrypted = gost_ofb(encrypted, K, IV)
+    decrypted = gost_ofb_decrypt(encrypted, K, IV)
     with open(decrypted_file, "wb") as file:
         file.write(decrypted)
 
@@ -75,7 +96,7 @@ if __name__ == "__main__":
     Режим шифрования: OFB
     """
 
-    _K = [random.randint(0, (2 ** 32) - 1) for i in range(8)]
-    _IV = random.randint(0, (2 ** 64) - 1)
+    _K = [random.randint(0, (1 << 32) - 1) for i in range(8)]
+    _IV = random.randint(0, (1 << 64) - 1)
 
     lab4("input.txt", "encrypted.txt", "decrypted.txt", _K, _IV)
